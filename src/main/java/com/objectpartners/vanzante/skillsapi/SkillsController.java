7591bd81c2324f78ed5c3c2f9198c7722d2fb912
Matthew Van Zante <mvanzante@gmail.com>
2018-06-13 22:02:14 -0500

    Initial commit - starter app from Spring Initializer with Web, JPA, Lombok, and H2 - SkillsController created

---
+++ src/main/java/com/objectpartners/vanzante/skillsapi/SkillsController.java
@@ -0,0 +1,10 @@
+package com.objectpartners.vanzante.skillsapi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mvanzante on 6/13/18.
+ */
+@RestController
+public class SkillsController {
+}
---
+++ src/main/java/com/objectpartners/vanzante/skillsapi/SkillsapiApplication.java
@@ -0,0 +1,12 @@
+package com.objectpartners.vanzante.skillsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsapiApplication.class, args);
+	}
+}
